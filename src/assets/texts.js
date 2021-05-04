8cd4f7ae7590263d453c8febed3c10cfdd6a61e5
Danillo Gonçalves Souza <danillosouza1704@gmail.com>
2021-05-03 21:08:47 -0300

    Update texts.js

--- src/assets/texts.js
+++ src/assets/texts.js
@@ -1,4 +1,4 @@
 const homePageText = "Selecione os items de forma que a mochila fique com o maior valor possível sem passar do peso máximo"
-const textLose = "Infelizmente, você não selecionou os melhores items, e o capatilismo  matou o Leon"
+const textLose = "Infelizmente, você não selecionou os melhores items, e o capitalismo  matou o Leon"
 const textWin = "Parabéns, você selecionou os melhores items, e agora Leon conseguiu vender tudo e comprar um novo Fiat Toro."
 export { homePageText, textLose, textWin }
